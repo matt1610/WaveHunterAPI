dca54d214fdaa36a1940b70a9c5d3f6a62861d32
matt1610 <mattstarkey@me.com>
2015-05-09 14:42:14 +0200

    Port Update

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-// var express = require('express');
-// var mongoose = require('mongoose');
-// var uriUtil = require('mongodb-uri');
-// var cors = require('cors');
-// var bodyParser = require('body-parser');
-// var photoController = require('./controllers/photoController.js');
-// var userController = require('./controllers/userController.js');
-// var authController = require('./controllers/authController.js');
-// var passport = require('passport');
+var express = require('express');
+var mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var photoController = require('./controllers/photoController.js');
+var userController = require('./controllers/userController.js');
+var authController = require('./controllers/authController.js');
+var passport = require('passport');
 
-// var DBURI = 'mongodb://mattstarkey:element12@ds031922.mongolab.com:31922/wavehunter';
-// var mongooseURI = uriUtil.formatMongoose(DBURI);
+var DBURI = 'mongodb://mattstarkey:element12@ds031922.mongolab.com:31922/wavehunter';
+var mongooseURI = uriUtil.formatMongoose(DBURI);
 
-// mongoose.connect(mongooseURI);
+mongoose.connect(mongooseURI);
 
-// var db = mongoose.connection;
+var db = mongoose.connection;
 
-// db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'));
 
-// var app = express();
+var app = express();
 
 
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
-// app.use(cors());
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(cors());
 
-// app.use(passport.initialize());
-// var router = express.Router();
+app.use(passport.initialize());
+var router = express.Router();
 
-// router.route('/photos').post(authController.isAuthenticated, photoController.postPhoto);
-// router.route('/getphotos').post(authController.isAuthenticated, photoController.getPhotos);
-// router.route('/users').post(userController.postUsers);
+router.route('/photos').post(authController.isAuthenticated, photoController.postPhoto);
+router.route('/getphotos').post(authController.isAuthenticated, photoController.getPhotos);
+router.route('/users').post(userController.postUsers);
 
-// app.use('/api', router);
+app.use('/api', router);
 
-// app.get('/test', function(req, res) {
-// 	res.send({isOn : true});
-// });
+app.get('/test', function(req, res) {
+	res.send({isOn : true});
+});
 
-// app.listen(5000);
-// console.log('Running on :5000');
+app.listen(process.env.PORT || 5000);
+console.log('Running on :5000');
 
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.PORT || 5000);
-console.log('Server running at http://127.0.0.1:1337/');
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(process.env.PORT || 5000);
+// console.log('Server running at http://127.0.0.1:1337/');
