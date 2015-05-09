19d64f5f1ccb2929d0d8e8103df569dcbdf71dc5
matt1610 <mattstarkey@me.com>
2015-05-09 14:40:19 +0200

    Port

--- index.js
+++ index.js
@@ -50,5 +50,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+}).listen(process.env.PORT || 5000);
 console.log('Server running at http://127.0.0.1:1337/');
