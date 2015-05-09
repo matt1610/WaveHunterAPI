7f2960d426de721e7f2b6f4d08420672f4a86bdf
matt1610 <mattstarkey@me.com>
2015-05-09 13:58:58 +0200

    removed bodyparser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 // var mongoose = require('mongoose');
 // var uriUtil = require('mongodb-uri');
 var cors = require('cors');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var photoController = require('./controllers/photoController.js');
 var userController = require('./controllers/userController.js');
 var authController = require('./controllers/authController.js');
@@ -20,11 +20,11 @@ var passport = require('passport');
 var app = express();
 
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -33,11 +33,11 @@ app.use(cors());
 app.use(passport.initialize());
 var router = express.Router();
 
-router.route('/photos').post(authController.isAuthenticated, photoController.postPhoto);
-router.route('/getphotos').post(authController.isAuthenticated, photoController.getPhotos);
-router.route('/users').post(userController.postUsers);
+// router.route('/photos').post(authController.isAuthenticated, photoController.postPhoto);
+// router.route('/getphotos').post(authController.isAuthenticated, photoController.getPhotos);
+// router.route('/users').post(userController.postUsers);
 
-app.use('/api', router);
+// app.use('/api', router);
 
 app.get('/test', function(req, res) {
 	res.send({isOn : true});
